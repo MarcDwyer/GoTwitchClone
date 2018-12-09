1bd05926e92318d72e84d8433ca325936fce2fd0
Marc Dwyer <envdia@gmail.com>
2018-12-08 21:31:47 -0600

    fixed likes and dislikes

---
+++ public/build/precache-manifest.10d6cf52468e713a76cd80b2679285fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d4f79b4e62bab42508cc",
+    "url": "/static/js/main.d4f79b4e.chunk.js"
+  },
+  {
+    "revision": "8bfc6b8874949490f57a",
+    "url": "/static/js/1.8bfc6b88.chunk.js"
+  },
+  {
+    "revision": "d4f79b4e62bab42508cc",
+    "url": "/static/css/main.ae3dfaae.chunk.css"
+  },
+  {
+    "revision": "fdad327731b57734e6d3291db75f962f",
+    "url": "/index.html"
+  }
+];
